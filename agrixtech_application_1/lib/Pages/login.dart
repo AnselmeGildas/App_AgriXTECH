514f67ad7cf7bb27a84543d326074a76ff2d6538
Forests4Future <evan.tuo@inphb.ci>
2023-05-22 13:21:43 +0000

    second commint test

--- agrixtech_application_1/lib/Pages/login.dart
+++ agrixtech_application_1/lib/Pages/login.dart
@@ -11,7 +11,7 @@ class SignUp extends StatelessWidget {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Text("Sign up",
+            Text("Sign up Page",
               style: TextStyle(
                 fontFamily: 'Poppins',
                 fontSize: 28
